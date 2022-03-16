4717b879b6ad2214f2c4a4209e5847d75cca41cd
Nikita <danshinnp@yandex.ru>
2022-03-16 12:19:33 +0500

    Add allure attachment

--- src/test/java/ToolsQaTest.java
+++ src/test/java/ToolsQaTest.java
@@ -1,52 +1,65 @@
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import pageObject.DemoQaRegisterForm;
 import utils.BaseTest;
 
+import java.io.ByteArrayInputStream;
+
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static utils.TestData.*;
 
-/**
- * Тесты для https://demoqa.com/.
- */
 public class ToolsQaTest extends BaseTest {
 
+    @Feature("Проверка DemoQa")
+    @Story("Проверка заполнения /automation-practice-form")
+    @DisplayName("Automation-practice-form")
+    @Owner("Danshin N.P.")
     @Test
     void FirstTest() {
         final DemoQaRegisterForm demoQaRegisterForm = new DemoQaRegisterForm();
-        open("/automation-practice-form");
 
-        //step_1
-        //fill form data
-        demoQaRegisterForm
-                .setFirstNameInput(firstName)
-                .setLastName(lastName)
-                .setUserEmail(email)
-                .setGender(gender)
-                .setMobile(mobile)
-                .setBirthDate(birthDateYear, birthDateMouth, birthDateDay)
-                .setSubjects(subjects)
-                .setHobbies(hobbies)
-                .setPicture(picture)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .submitForm();
+        step("Открытие формы", () -> {
+            open("/automation-practice-form");
+        });
 
-        //step_2
-        //Check results
-        demoQaRegisterForm
-                .checkResultInModal("Student Name", firstName + " " + lastName)
-                .checkResultInModal("Student Email", email)
-                .checkResultInModal("Gender", gender)
-                .checkResultInModal("Mobile", mobile)
-                .checkResultInModal("Date of Birth", birthDate)
-                .checkResultInModal("Mobile", mobile)
-                .checkResultInModal("Subjects", subjects)
-                .checkResultInModal("Hobbies", String.join(", ", hobbies))
-                .checkResultInModal("Picture", picture.getName())
-                .checkResultInModal("Address", address)
-                .checkResultInModal("State and City", state + " " + city);
+        step("Заполнение формы", () -> {
+            demoQaRegisterForm
+                    .setFirstNameInput(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(email)
+                    .setGender(gender)
+                    .setMobile(mobile)
+                    .setBirthDate(birthDateYear, birthDateMouth, birthDateDay)
+                    .setSubjects(subjects)
+                    .setHobbies(hobbies)
+                    .setPicture(picture)
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .submitForm();
+        });
+
+        step("Проверка значений формы", () -> {
+            demoQaRegisterForm
+                    .checkResultInModal("Student Name", firstName + " " + lastName)
+                    .checkResultInModal("Student Email", email)
+                    .checkResultInModal("Gender", gender)
+                    .checkResultInModal("Mobile", mobile)
+                    .checkResultInModal("Date of Birth", birthDate)
+                    .checkResultInModal("Mobile", mobile)
+                    .checkResultInModal("Subjects", subjects)
+                    .checkResultInModal("Hobbies", String.join(", ", hobbies))
+                    .checkResultInModal("Picture", picture.getName())
+                    .checkResultInModal("Address", address)
+                    .checkResultInModal("State and City", state + " " + city);
+        });
     }
-
-
 }
--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,12 +1,38 @@
 package utils;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
-  @BeforeAll
-  static void beforeAll() {
-    Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browserSize = "1920x1080";
-  }
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+
+    }
 }
